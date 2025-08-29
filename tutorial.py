44a6eca73aba07926b69dd3eaab07c9be5580b0e
Rishit Shah <aryanrishit1@gmail.com>
2025-08-29 13:11:53 +0530

    Commit 5 [55:37]: Implemented the orbit drawing code to see the planets' orbits. Note: For commit 4, time should be 51:59.

--- tutorial.py
+++ tutorial.py
@@ -40,6 +40,16 @@ class Planet:
         y = self.y * self.SCALE + HEIGHT / 2
         # here width / 2 and height / 2 makes the values are centered
 
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                x, y = point
+                x = x * self.SCALE + WIDTH / 2
+                y = y * self.SCALE + HEIGHT / 2
+                updated_points.append((x, y))
+
+            pygame.draw.lines(win, self.color, False, updated_points, 2)
+
         pygame.draw.circle(win, self.color, (x, y), self.radius)
 
     def attraction(self, other):
